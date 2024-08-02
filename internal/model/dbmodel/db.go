ee75369d0e5f782c61485fa6e4d393995d71f256
sergeysuprunchuk <sergeysuprunchuk@yandex.ru>
2024-08-02 15:43:28 +0700

    добавил возможность получить список таблиц базы данных

--- internal/model/dbmodel/db.go
+++ internal/model/dbmodel/db.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"datapoint/pkg/database"
 	"fmt"
+	sq "github.com/Masterminds/squirrel"
 	"github.com/google/uuid"
 )
 
@@ -95,4 +96,99 @@ func (db *DB) ExecContext(ctx context.Context, query string, args ...any) (sql.R
 	}
 
 	return db.db.ExecContext(ctx, query, args...)
+}
+
+type FK struct {
+	TableName  string
+	ColumnName string
+}
+
+type Column struct {
+	Name       string
+	Type       string
+	IsRequired bool
+	IsPK       bool
+	FK         *FK
+}
+
+type Table struct {
+	Name       string
+	ColumnList []*Column
+}
+
+func (db *DB) tableList(ctx context.Context, where sq.Sqlizer) ([]*Table, error) {
+	rows, err := db.db.B.Select(
+		"c.table_name",
+		"c.column_name",
+		"c.data_type",
+		"c.is_nullable = 'NO' AND c.column_default IS NULL",
+		"tc.constraint_type",
+		"kcu2.table_name",
+		"kcu2.column_name",
+	).From("information_schema.columns c").
+		LeftJoin("information_schema.key_column_usage kcu USING (table_name, column_name)").
+		LeftJoin("information_schema.table_constraints tc USING (constraint_name)").
+		LeftJoin("information_schema.referential_constraints rc USING (constraint_name)").
+		LeftJoin("information_schema.key_column_usage kcu2 ON rc.unique_constraint_name = kcu2.constraint_name").
+		Where("c.table_schema = 'public'").
+		Where(where).
+		OrderBy("c.table_name", "c.ordinal_position").
+		QueryContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = rows.Close() }()
+
+	var (
+		tableList []*Table
+		lastT     *Table
+		lastC     *Column
+	)
+
+	for rows.Next() {
+		var (
+			t                    = new(Table)
+			c                    = new(Column)
+			constraint, fkT, fkC *string
+		)
+
+		if err = rows.Scan(&t.Name, &c.Name, &c.Type, &c.IsRequired, &constraint, &fkT, &fkC); err != nil {
+			return nil, err
+		}
+
+		if lastT == nil || lastT.Name != t.Name {
+			lastT = t
+			tableList = append(tableList, t)
+		}
+
+		if lastC == nil || lastC.Name != c.Name {
+			lastC = c
+			lastT.ColumnList = append(lastT.ColumnList, c)
+		}
+
+		if constraint != nil && *constraint == "PRIMARY KEY" {
+			lastC.IsPK = true
+		} else if constraint != nil && *constraint == "FOREIGN KEY" && fkT != nil && fkC != nil {
+			lastC.FK = &FK{TableName: *fkT, ColumnName: *fkC}
+		}
+	}
+
+	return tableList, nil
+}
+
+func (db *DB) TableList(ctx context.Context) ([]*Table, error) {
+	return db.tableList(ctx, nil)
+}
+
+func (db *DB) TableByName(ctx context.Context, name string) (*Table, error) {
+	tableList, err := db.tableList(ctx, sq.Eq{"c.table_name": name})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(tableList) == 0 {
+		return nil, fmt.Errorf("таблицы с именем %s не существует", name)
+	}
+
+	return tableList[0], nil
 }
